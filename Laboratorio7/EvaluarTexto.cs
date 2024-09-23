e81e582049bd9203c7246ffbef11635c6a1d1c40
danie <danie@MARIN>
2024-09-23 10:35:51 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorio7/EvaluarTexto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio7
+{
+    public class EvaluarTexto
+    {
+        static bool Apertura(char simbolo)
+        {
+            return simbolo == '(' || simbolo == '[' || simbolo == '{';
+        }
+
+        static bool Cierre(char simbolo)
+        {
+            return simbolo == ')' || simbolo == ']' || simbolo == '}';
+        }
+
+        static bool Completo(char apertura, char cierre)
+        {
+            return (apertura == '(' && cierre == ')') ||
+                   (apertura == '[' && cierre == ']') ||
+                   (apertura == '{' && cierre == '}');
+        }
+
+        public bool Balanceado(string texto)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char simbolo in texto)
+            {
+                if (Apertura(simbolo))
+                {
+                    stack.Push(simbolo);
+                }
+                else if (Cierre(simbolo))
+                {
+                    if (stack.Count == 0 || !Completo(stack.Pop(), simbolo))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ Laboratorio7/Program.cs
@@ -0,0 +1,62 @@
+﻿using Laboratorio7;
+using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        EvaluarTexto evaluarTexto = new EvaluarTexto();
+        int cat = 0;
+        while (cat == 0)
+        {
+            try
+            {
+                Console.WriteLine("Escriba su texto");
+                string texto = Console.ReadLine();
+                Console.WriteLine("¿Desea evaluarlo?");
+                Console.WriteLine("1. Si");
+                Console.WriteLine("2. No (Salir del programa)");
+                int opcion = int.Parse(Console.ReadLine());
+                
+                switch (opcion)
+                {
+                    case 1:
+                        if (evaluarTexto.Balanceado(texto))
+                        {
+                            Console.WriteLine("Está balanceado");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No está balanceado");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Has salido del programa.");
+                        cat = 1;
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Inténtalo de nuevo con una de las opciones.");
+                        Console.WriteLine("Presiona una tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                }
+
+
+            }
+            catch (FormatException ex)
+            {
+                Console.Clear();
+                Console.WriteLine("Ha ocurrido un error de formato. " + ex.Message);
+                Console.WriteLine("Presiona una tecla para continuar...");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine("Ha ocurrido un error. " + ex.Message);
+                Console.WriteLine("Presiona una tecla para continuar...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
